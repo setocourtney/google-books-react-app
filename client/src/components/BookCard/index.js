e315112145763ebe513b0d89172c608e22543382
setocourtney <courtneyjseto@gmail.com>
2020-05-28 15:48:28 -0700

    fixed bookcard title

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -7,8 +7,8 @@ const BookCard = (props) => {
 
           <div className="card-image">
             <img src={ props.book.image ? props.book.image : "https://img.jakpost.net/c/2019/03/02/2019_03_02_66706_1551461528._large.jpg"}></img>
-            <h4 className="card-title" style={{ opacity: .6 }}>
-              <a className=" amber lighten-5 waves-effect btn-flat" href={ props.book.link ? props.book.link : "https://books.google.com/" }>{ props.book.title ? props.book.title : "Book Title" }</a>
+            <h4 className="card-title amber lighten-5" style={{ opacity: .6 }}>
+              <a className="black-text waves-effect" href={ props.book.link ? props.book.link : "https://books.google.com/" }>{ props.book.title ? props.book.title : "Book Title" }</a>
             </h4>
             <button className="btn-floating halfway-fab waves-effectbtn" onClick={props.handleSubmit} style={ { bottom: "5px" }}>
                 <i className="material-icons left red">{ props.saved ? "remove" : "add" }</i>
